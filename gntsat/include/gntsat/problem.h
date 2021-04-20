135ea45411063c9bd3d5bff8b0b029a27c42937b
Xiaoyue Chen <xiaoyue_chen@outlook.com>
2021-04-20 15:49:49 +0200

    Add problem data structure

---
+++ gntsat/include/gntsat/problem.h
@@ -0,0 +1,12 @@
+#include <array>
+#include <vector>
+
+namespace gntsat {
+
+struct Problem {
+  using Clause = std::array<int, 3>;
+  int var_count;
+  std::vector<Clause> cnf;
+};
+
+}  // namespace gntsat
