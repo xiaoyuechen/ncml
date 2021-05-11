f8791259f316969679e7dfab2d5f2af448a9ae17
Xiaoyue Chen <xiaoyue_chen@outlook.com>
2021-05-11 19:54:04 +0200

    Add population printing

--- gntsat/src/main.cc
+++ gntsat/src/main.cc
@@ -1,4 +1,7 @@
+#include <algorithm>
+
 #include "gntsat/compute.h"
+#include "gntsat/io.h"
 #include "gntsat/parser.h"
 
 int main(int argc, const char* argv[]) {
@@ -6,7 +9,7 @@ int main(int argc, const char* argv[]) {
   srand(time(0));
   auto problem = gntsat::readfile(argv[1]);
 
-  auto population = CreatePopulation(1024, problem.var_count);
+  auto population = CreatePopulation(20, problem.var_count);
 
   constexpr size_t best_size = 16;
   size_t bests[best_size];
@@ -16,6 +19,28 @@ int main(int argc, const char* argv[]) {
       (uint64_t*)_mm_malloc((population.num_var + 63) / 64 * 8, 8);
 
   while (true) {
+    size_t best_individual_offset;
+    size_t most_sat = 0;
+    for (size_t i = 0; i < population.size * population.num_var;
+         i += population.num_var) {
+      size_t sat = CountSat(population.individuals, i, cnf_begin, cnf_end);
+      if (sat > most_sat) {
+        most_sat = sat;
+        best_individual_offset = i;
+      }
+    }
+    printf("Best %d\n", (int)most_sat);
+    PrintBitstring(population.individuals, best_individual_offset,
+                   population.num_var);
+    printf("\n");
+
+    for (size_t i = 0; i < population.size * population.num_var;
+         i += population.num_var) {
+      PrintBitstring(population.individuals, i, population.num_var);
+      if (i / population.num_var == population.newest) printf("<<======");
+      printf("\n");
+    }
+
     TournamentSelect(bests, population, best_size, 64, cnf_begin, cnf_end);
     size_t parentx = rand() % best_size;
     size_t parenty = rand() % best_size;
